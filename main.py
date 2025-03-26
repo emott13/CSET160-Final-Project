51d5e592eeaad16b076f9b43e6bcc100c94bba7a
bluemario8 <bluemario812@gmail.com>
2025-03-26 09:50:00 -0400

    Added main.py with flask template

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+
+app = Flask(__name__)
+conn_str = "mysql://root:cset155@localhost/boatdb"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+@app.route("/")
+def home():
+    return "home page"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

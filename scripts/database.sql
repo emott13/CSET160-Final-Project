0cd189dae4027ef548ffe0ba78bb9fb3ba33e16b
EllieMott <elizabethmott1326@gmail.com>
2025-03-27 08:55:15 -0400

    +database.sql

---
+++ scripts/database.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE IF NOT EXISTS cset160final;
+USE cset160final;
+CREATE TABLE IF NOT EXISTS students(
+	student_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    email VARCHAR(40),
+    password VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS teachers(
+	teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    email VARCHAR(40),
+    password VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS tests(
+	test_id INT AUTO_INCREMENT PRIMARY KEY,
+    teacher_id INT NOT NULL
+);
+
+ALTER TABLE teachers AUTO_INCREMENT=90000;
+ALTER TABLE students AUTO_INCREMENT=10000;
+ALTER TABLE tests AUTO_INCREMENT=780000;
+
+INSERT INTO teachers (first_name, last_name, email)
+VALUES
+	('John', 'Smith', 'jsmith@gradehub.com'), ('jane', 'Doe', 'jdoe@gradehub.com'),
+	("Irene", "Marchand", "imarchand@gradehub.com"), ("Aishwarya", "Midgley", "amidgley@gradehub.com"),
+	("Kamila", "Anselmi", "kanselmi@gradehub.com"), ("Iael", "Nussenbaum", "inussenbaum@gradehub.com"),
+	("Kalliope", "Mazur", "kmazur@gradehub.com"), ("Shana ", "Michaels", "smichaels@gradehub.com"),
+	("Josua", "Kavaliou", "jkavaliou@gradehub.com"), ("Brynjarr", "Carey", "bcarey@gradehub.com"),
+	("Domitilla", "Sudworth", "dsudworth@gradehub.com"), ("Zach", "Cvetkovic", "zcvetkovic@gradehub.com"),
+	("Shou", "Le", "sle@gradehub.com"), ("Isaac", "Myers", "imyers@gradehub.com"),
+	("Maja", "Ismailova", "mismailova@gradehub.com"), ("Darina", "Polakova", "dpolakova@gradehub.com"),
+	("Sujay", "Petocs", "spetocs@gradehub.com"), ("Mirka", "MacNeill", "mmacneill@gradehub.com"),
+	("Dayna", "Assenberg", "dassenberg@gradehub.com"), ("Daedalus", "Dziedzic", "ddziedzic@gradehub.com"),
+	("Janvier", "Nakamura", "jnakamura@gradehub.com"), ("Erramun", "Xu", "exu@gradehub.com"),
+	("Boitumelo", "Gomulka", "bgomulka@gradehub.com"), ("Emma", "Sedlackova", "esedlackova@gradehub.com"),
+	("Danijela", "Zsoldos", "dzsoldos@gradehub.com"), ("Gavin", "Bryan", "gbryan@gradehub.com");
+
+INSERT INTO students(first_name, last_name, email)
+VALUES
+	('Bogomilu','Willard','bwillard@gradehub.com'), ('Junius','Dannel','jdannel@gradehub.com'), 
+    ('Sashi','Bazzoli','sbazzoli@gradehub.com'), ('Carlo','Araullo','caraullo@gradehub.com'), 
+    ('Tancred','Yurchenko','tyurchenko@gradehub.com'), ('Caelia','Ramos','cramos@gradehub.com'), 
+    ('Imogene','Jesus','ijesus@gradehub.com'), ('Mary','Colt','mcolt@gradehub.com'), 
+    ('Jaak','Baumer','jbaumer@gradehub.com'), ('Stacia','Bellerose','sbellerose@gradehub.com'), 
+    ('Gemini','Kadyrov','gkadyrov@gradehub.com'), ('Laurynas','Sheehy','lsheehy@gradehub.com'), 
+    ('Sandra','McGee','smcgee@gradehub.com'), ('Thomas','Garey','tgarey@gradehub.com'), 
+    ('Elwira','Fukui','efukui@gradehub.com'), ('Hakon','Graf','hgraf@gradehub.com'), 
+    ('Isaac','Macnamara','imacnamara@gradehub.com'), ('Anna','Bosch','abosch@gradehub.com'), 
+    ('Annelies','Leonardsen','aleonardsen@gradehub.com'), ('Celeste','Rademaker','crademaker@gradehub.com'), 
+    ('Ildo','Schroder','ischroder@gradehub.com'), ('Andre','Diaz','adiaz@gradehub.com'), 
+    ('Nelda','Kuznetsova','nkuznetsova@gradehub.com'), ('Maggie','Addisons','maddisons@gradehub.com'), 
+    ('Joanna','Schroder','jshroder@gradehub.com'), ('Merja','Avci','mavci@gradehub.com');
+
+SELECT * FROM teachers;
+SELECT * FROM students;

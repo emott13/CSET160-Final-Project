269c568e99dab08eb860b2b2eb6f4d202e5ddbd3
bluemario8 <bluemario812@gmail.com>
2025-03-27 08:05:49 -0400

    Changed username to email in login and signup and added fname and lname to signup

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def login():
 def signup():
     return render_template("signup.html")
 
+@app.route("/signup", methods=["POST"])
+def signupPost():
+    return render_template("signup.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
